c36ee7700f2ef20641314da2e86337ce2e60b515
Adarsh Jaiswal <129786062+AdarshJaiswal031@users.noreply.github.com>
2026-01-19 19:50:41 +0530

    Update ternary_search_searching.java

--- ternary_search_searching.java
+++ ternary_search_searching.java
@@ -1,14 +1,15 @@
-    private double ternarySearch(){
-        double precision=1e-9;
+  private double ternarySearch(){
         double l=0;
         double r=1e9;
-        while((r-l)>precision){
+        int ite=0;
+        while(ite<200){
             double m1=l+((r-l)/3);
             double m2=r-((r-l)/3);
             double fm1=f(m1);
             double fm2=f(m2);
             if(fm1<fm2) l=m1;
             else r=m2;
+            ite++;
         }
         return f(l);
     }
